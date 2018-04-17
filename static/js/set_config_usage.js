e5c2cdbbb1e35ce3465517ae23dec1606cc67b88
AnnaBelle95 <afiqah239@gmail.com>
2018-04-17 16:40:08 +0800

    1

---
+++ static/js/set_config_usage.js
@@ -0,0 +1,16 @@
+// Set the messenger to open fullscreen
+FxoMessenger.setConfig({
+  fullscreen: true
+});
+
+// Set header text and color
+FxoMessenger.setConfig({
+  headerText: 'Chat to Us',
+  color: '#4B0082'
+});
+
+// Set config then open the messenger
+FxoMessenger.setConfig({
+  fullscreen: true
+});
+FxoMessenger.create();
