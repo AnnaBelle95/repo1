a7fe2ba339e169a14b691b6836f947f09583662b
AnnaBelle95 <afiqah239@gmail.com>
2018-04-17 13:41:01 +0800

    1

--- static/js/init.js
+++ static/js/init.js
@@ -57,3 +57,4 @@
         }
     });
 })(jQuery);
+	<script src="https://widget.flowxo.com/embed.js" data-fxo-widget="eyJ0aGVtZSI6IiNmMzdhNzEiLCJ0ZWxlZ3JhbSI6eyJib3RVc2VybmFtZSI6IkZsaWdodEJvdCJ9LCJ3ZWxjb21lVGV4dCI6IldlbGNvbWUgdG8gbXkgQ2hhdEJvdCEifQ==" async defer></script>;
